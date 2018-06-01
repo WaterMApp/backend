6d3b613ecb21380b64f62a70b54100ae3eb6ddb9
Be-P <bp@vps506803.ovh.net>
2018-06-01 02:00:26 +0200

    update

--- index.js
+++ index.js
@@ -30,14 +30,16 @@ app.get("/", function(req,res){
 });
 
 app.get("/fountain_list",function(req,res){
+    console.log("get fountain_list");
     db.get_fountains(
-        (f)=>res.send(JSON.stringify(f))
+        (f)=>{console.log(f); res.send(JSON.stringify(f));}
     );
 
 });
 
 app.get("/fountain_data/:id",function(req,res){
-    
+    log("get fountain_data");
+
     db.get_last_measurement(
         req.params.id,
         (m)=>res.send(JSON.stringify(m))
@@ -46,16 +48,16 @@ app.get("/fountain_data/:id",function(req,res){
 });
 
 app.get("/fountain_comments/:id",function(req,res){
-    
+    log("get fountain_comments");
     db.get_comments(
         req.params.id,
-        (l)=>{ res.send(JSON.stringify(l)) }
+        (l)=>{ log(l);res.send(JSON.stringify(l)) }
     );
 
 });
 
 app.post("/fountain_create",function(req,res){
-        
+        log("post fountain_create");
         if(!isFloat(req.body.lat) || !isFloat(req.body.lng)) {
             res.send("malformed request body");
             return;
@@ -69,23 +71,56 @@ app.post("/fountain_create",function(req,res){
         res.send(req.body.id);
 });
 
-// @TODO CHECK MALFORMED MESSAGES
 app.post("/fountain_comments/:id",function(req,res){
-    
+    log("post fountain_comments");
+
+    if(!req.body.text || 
+       !(typeof req.body.text === 'string') || 
+       req.body.text.length<=0)
+    {
+        res.send("malformed request");return;
+    }
+
+    if(!req.body.name || 
+       !(typeof req.body.name === 'string') || 
+       req.body.name.length<=0)
+    {
+        res.send("malformed request");return;
+    }
+    log(req.params);
+    log(req.params.id); 
     req.body.f_id=req.params.id;
+    
     if(!req.body.timestamp) req.body.timestamp = getTimestamp();
+
+    log("adding new comment",req.body);
 
     db.write_comment(req.body);
-
+    res.send("ok")
 });
 
-// @TODO CHECK MALFORMED MESSAGES
 app.post("/fountain_data/:id", function(req,res){
+    console.log("post fountain data")
     // add measurement to db
     req.body.f_id=req.params.id
+    
+    log(req.body); 
+    if(!isInt(req.body.ph) && !isFloat(req.body.ph)){
+        res.send("malformed ph");return;
+    }
+    if(!isInt(req.body.turb) && !isFloat(req.body.turb)){
+        res.send("malformed turb");return;
+
+    }
+    if(!isInt(req.body.temp) && !isFloat(req.body.temp)){
+        res.send("malformed temp");return;
+    }
+
     if(!req.body.timestamp) req.body.timestamp = getTimestamp();
 
+    log("writing measurement")
     db.write_measurement(req.body)
+    res.send("ok")
 });
 
 
